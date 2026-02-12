2a68ab28a5ddf063a3dc7d07c151cb09e924b2c9
pr1t4m-d3y <cooldeypritam@gmail.com>
2026-02-13 02:53:32 +0530

    Initial Commit

---
+++ PocketFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PocketFlow
+//
+//  Created by Aditya Kumar on 13/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PocketFlow/PocketFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketFlowApp.swift
+//  PocketFlow
+//
+//  Created by Aditya Kumar on 13/02/26.
+//
+
+import SwiftUI
+
+@main
+struct PocketFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
